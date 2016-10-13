25cf6c2f86e9c174108f25e7eafb1b26761bec86
Daniel Bridgman <gd45daniel@vfs.local>
2016-10-12 22:02:03 -0700

    Combat interface
    First Draft to Combat interface

--- Wille_T1_FinalProject/Program.cs
+++ Wille_T1_FinalProject/Program.cs
@@ -24,11 +24,33 @@ namespace Wille_T1_FinalProject
         //introduction for the player and setting up their Monster
         static void Start()
         {
+
             Combat(oPlayerMonster, iBattleNumber);
         }
-        static void Combat()
+
+        static void showFightStats(Monster PlayerMonster, Monster EnemyMonster)
         {
+            Console.WriteLine(" ____________________________                      {0}'s Stats" , EnemyMonster.GetName());
+            Console.WriteLine("|         Name:{0}           |");
+            Console.WriteLine("|                            |                     attack: {1}", EnemyMonster.GetAttack());
+            Console.WriteLine("|        Health:{0}          |                    defence: {0}", EnemyMonster.GetDefense(), EnemyMonster.GetHealth());
+            Console.WriteLine("|                            |                      Speed: {0}", EnemyMonster.GetSpeed());
+            Console.WriteLine("|___________________________ |                    trinity: {0}", EnemyMonster.GetStringTrinity());
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("          {0}'s stats                  ____________________________", EnemyMonster.GetName());
+            Console.WriteLine("                                      |          Name:{0}          |", PlayerMonster.GetName());
+            Console.WriteLine("          attack: {1}                 |                            |", PlayerMonster.GetAttack());
+            Console.WriteLine("         defence: {1}                 |        Health:{2}          |", PlayerMonster.GetDefense(), PlayerMonster.GetHealth());
+            Console.WriteLine("           Speed: {1}                 |                            |", PlayerMonster.GetSpeed());
+            Console.WriteLine("         trinity: {0}                 | ___________________________|", PlayerMonster.GetStringTrinity());
+        }
+
+        static void Combat(Monster playerMonster, int iBattleNumber)
+        {
+            Monster enemyMonster = new Monster(iBattleNumber);
+            showFightStats(playerMonster, enemyMonster, iBattleNumber);
             
+            Console.ReadLine();
         }
     }
 }
